7da74f80958954891bfc4b59d4e110d25067349d
院长 <32212075+yuanter@users.noreply.github.com>
2025-01-04 01:53:44 +0800

    Create envir.js

---
+++ util/envir.js
@@ -0,0 +1,20 @@
+window = global;
+ 
+location = { 
+    "ancestorOrigins": {}, 
+    "href": "https://y.qq.com/n/ryqq/search?w=%E5%91%A8%E6%9D%B0%E4%BC%A6",
+    "origin": "https://y.qq.com",
+    "protocol": "https:",
+    "host": "y.qq.com",
+    "hostname": "y.qq.com",
+    "port": "", 
+    "pathname": "/n/ryqq/search",
+    "search": "?w=%E5%91%A8%E6%9D%B0%E4%BC%A6",
+    "hash": ""
+}
+ 
+navigator = { 
+    appCodeName: "Mozilla",
+    appName: "Netscape",
+    appVersion: "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
